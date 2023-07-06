b335935ee60751c2b5916d7d7572a006e926394d
Vinnie <121957015+VinnieDavid@users.noreply.github.com>
2023-07-06 19:45:12 +0000

    pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home() {
+    return <h1>Esse pedacinho que construi por nos meu amor,
+         é apenas um começo de algo muito bom para nos !! 
+         TE AMOOOOOOO !!! </h1>
+}
+
+export default Home;
