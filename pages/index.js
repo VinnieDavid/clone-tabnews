1156fd24ad0cc8013f9109360f9c422a3003d912
Vinnie <121957015+VinnieDavid@users.noreply.github.com>
2023-07-07 17:19:19 +0000

    nova mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     return <h1>Esse pedacinho que construi por nos meu amor,
          é apenas um começo de algo muito bom para nos !! 
-         TE AMOOOOOOO !!! muitoooooooo </h1>
+         TE AMOOOOOOO !!! muitoooooooo S2 s2 </h1>
 }
 
 export default Home;
